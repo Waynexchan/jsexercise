118fc236e9d22dae0566812b216e1004f264ef46
wayne chan <waynestock666@gmail.com>
2024-05-23 08:45:50 +0100

    upload

--- exerciseAsyncAwait2.js
+++ exerciseAsyncAwait2.js
@@ -8,6 +8,9 @@ async function fetchStarship() {
 }
 
 // --------- Answer:
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
 
 
 
@@ -28,6 +31,15 @@ const getData = async function () {
 };
 
 // --------- Answer:
+  const [users, posts, albums] = await Promise.all(
+    urls.map(async function(url) {
+      const response = await fetch(url);
+      return response.json();
+    }))
+
+  console.log("users", users);
+  console.log("posta", posts);
+  console.log("albums", albums);
 
 
 
@@ -38,4 +50,5 @@ const data = await response.json();
 console.log(data);
 //!! It will actually give you an error: "await is only valid in async functions and the top level bodies of modules"
 //WHY?
-
+// ANSWER: Top Level Await ONLY works inside of a module. We were able to run this code in the chrome dev tools
+// because inside the console, it technically runs like a module. 
